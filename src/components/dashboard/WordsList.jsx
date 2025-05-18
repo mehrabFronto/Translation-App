50b8977d9d47f3c4f24d6935ee64e5548e943aff
mehrabFronto <mehrabd20@gmail.com>
2025-05-18 23:43:48 +0330

    refactor(dashboard): implement drag handle using external ref for sortable items

--- src/components/dashboard/WordsList.jsx
+++ src/components/dashboard/WordsList.jsx
@@ -31,9 +31,9 @@ const WordsList = () => {
 
   const handleDragEnd = (event) => {
     const { active, over } = event;
-    if (active.id !== over.id) {
-      const oldIndex = words.findIndex((item, i) => i === +active.id);
-      const newIndex = words.findIndex((item, i) => i === +over.id);
+    if (active?.id !== over?.id) {
+      const oldIndex = words.findIndex((_, i) => i === +active.id);
+      const newIndex = words.findIndex((_, i) => i === +over.id);
       const newWords = arrayMove(words, oldIndex, newIndex);
       setWords(newWords);
     }
@@ -50,18 +50,36 @@ const WordsList = () => {
         items={words.map((_, i) => i)}
         strategy={verticalListSortingStrategy}
       >
-        <div className="px-2 py-2 max-h-[210px] overflow-y-auto overflow-x-hidden space-y-2">
-          {words &&
-            words.map(({ persianLabel, englishLabel }, index) => {
-              const value =
-                selectedLanguage.type === "persian"
-                  ? persianLabel
-                  : englishLabel;
-              const hasError = isEmpty(value);
+        <div className="px-2 py-2 max-h-[190px] overflow-y-auto overflow-x-hidden space-y-2">
+          {words.map(({ persianLabel, englishLabel }, index) => {
+            const value =
+              selectedLanguage.type === "persian" ? persianLabel : englishLabel;
+            const hasError = isEmpty(value);
 
-              return (
-                <SortableItem key={index} id={index}>
-                  <div className="border-b border-gray-200 last:border-b-0 px-3 py-4 rounded-md flex items-center justify-between gap-4 bg-white shadow-sm">
+            return (
+              <SortableItem key={index} id={index}>
+                {(ref, listeners, attributes, isDragging) => (
+                  <div className="border-b border-gray-200 last:border-b-0 pr-3 rounded-md flex items-center justify-between gap-4 bg-white shadow-sm overflow-hidden">
+                    <span
+                      className={`block pl-3 pr-6 py-4 ${
+                        isDragging ? "cursor-grabbing" : "cursor-grab"
+                      }`}
+                      ref={ref}
+                      {...listeners}
+                      {...attributes}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                        className="w-5 h-5"
+                      >
+                        <path
+                          fill="black"
+                          d="M32 288c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 288zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z"
+                        />
+                      </svg>
+                    </span>
+
                     <p
                       className={`font-medium ${
                         hasError ? "text-red-500" : ""
@@ -86,9 +104,10 @@ const WordsList = () => {
                       placeholder=". . . . ."
                     />
                   </div>
-                </SortableItem>
-              );
-            })}
+                )}
+              </SortableItem>
+            );
+          })}
         </div>
       </SortableContext>
     </DndContext>
@@ -96,29 +115,24 @@ const WordsList = () => {
 };
 
 const SortableItem = ({ id, children }) => {
-  const { attributes, listeners, setNodeRef, transform, isDragging } =
-    useSortable({ id });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    isDragging,
+  } = useSortable({ id });
 
   const style = {
     transform: CSS.Transform.toString(transform),
-    transition: transform ? "transform 200ms ease" : null,
-    cursor: isDragging ? "grabbing" : "grab",
+    transition: transform ? "transform 200ms ease" : undefined,
     touchAction: "none",
   };
 
-  const isInput = (e) => {
-    return e.target.tagName === "INPUT" || e.target.closest("input") !== null;
-  };
-
-  const filteredListeners = {
-    onPointerDown: (e) => {
-      if (!isInput(e)) listeners.onPointerDown?.(e);
-    },
-  };
-
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...filteredListeners}>
-      {children}
+    <div ref={setNodeRef} style={style}>
+      {children(setActivatorNodeRef, listeners, attributes, isDragging)}
     </div>
   );
 };
